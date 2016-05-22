00fa7f23015616d7fd9d682086ca2fd78e7ac8bd
Kogut Vladimir <kogutvs@gmail.com>
2016-05-22 16:07:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿//Дана строка символов. Необходимо проверить, является ли эта строка палиндромом.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String S;
+            Console.WriteLine("Введите строку для проверки её на палиндромность.");
+            S = Console.ReadLine();
+            char[] Marks = { ' ', '.', ',', '!', ':', ';', '-', '?' };
+            String[] Temp = new String[S.Length];
+            Temp = S.Split(Marks);
+            S = String.Join(null, Temp);
+            S = S.ToLower();
+            Boolean Flag = false;
+            for(int i = 0, j = S.Length-1; i<S.Length && j>=0; i++, j--)
+            {
+                if(S[i] != S[j])
+                {
+                    Flag = false;
+                    break;
+                }
+                Flag = true;
+            }
+            if(Flag)
+            {
+                Console.WriteLine("Это палиндром");
+            } else
+            {
+                Console.WriteLine("Это не палиндром");
+            }
+        }
+    }
+}
